c99f96ff25e6d677a1c6f704bc19987084d7094a
Scclegionx <147328046+Scclegionx@users.noreply.github.com>
2025-01-20 20:38:11 +0700

    fix

--- chat-service/src/main/java/chat_service/chat_service/ChatServiceApplication.java
+++ chat-service/src/main/java/chat_service/chat_service/ChatServiceApplication.java
@@ -1,11 +1,9 @@
 package chat_service.chat_service;
 
-import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableMongock
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
